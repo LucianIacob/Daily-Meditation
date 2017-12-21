c878791e32446ae07e23e361ffd9e14fa091d92e
Lucian Iacob <lucian.iacob@endava.com>
2017-12-21 16:41:26 +0200

    Added reporting manager & utils

---
+++ mobile/src/main/java/com/dailymeditation/android/reporting/ReportingEvent.java
@@ -0,0 +1,21 @@
+package com.dailymeditation.android.reporting;
+
+/**
+ * Created by Lucian Iacob.
+ * Cluj-Napoca, 21 December, 2017.
+ */
+
+public enum ReportingEvent {
+
+    LOAD_VERSE,
+    CLICK_SHARE,
+    OPEN_FEEDBACK,
+    SENT_FEEDBACK,
+    ERROR_INTERSTITIAL,
+    OPEN_INTERSTITIAL,
+    CLICK_INTERSTITIAL,
+    VIEW_WIDGET,
+    REMOVE_WIDGET,
+    INSTALL_WIDGET
+
+}
